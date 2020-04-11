78e4dd3065f02256f8e8e2aa87dc6aff89fa732c
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-11 19:27:12 +0700

    Hello

---
+++ README.md
@@ -0,0 +1,3 @@
+"# TranThanhHai_59130561_HelloWord" 
+"# TranThanhHai_59130561" 
+"# TranThanhHai_59130561" 
---
+++ app/src/main/java/vn/edu/ntu/thanhhai/thanhhai_59130561_helloword/MainActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.ntu.thanhhai.thanhhai_59130561_helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello Word");
+    }
+}
